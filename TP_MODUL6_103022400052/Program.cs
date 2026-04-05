234d3c2464496df8aa1d031a03cf52f7d679d571
Faliqaqil <faliq.aqil.ra@gmail.com>
2026-04-05 18:29:32 +0700

    commit message

--- TP_MODUL6_103022400052/Program.cs
+++ TP_MODUL6_103022400052/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace TP_MODUL6_NIM
 {
@@ -10,6 +11,9 @@ namespace TP_MODUL6_NIM
 
         public SayaMusicTrack(string title)
         {
+            Debug.Assert(title != null, "Judul tidak boleh null.");
+            Debug.Assert(title.Length <= 100, "Judul maksimal 100 karakter.");
+
             this.title = title;
             Random random = new Random();
             this.id = random.Next(10000, 100000);
@@ -18,36 +22,65 @@ namespace TP_MODUL6_NIM
 
         public void IncreasePlayCount(int count)
         {
-            this.playCount += count;
+            Debug.Assert(count <= 10000000, "Input penambahan maksimal 10.000.000.");
+
+            try
+            {
+                checked { this.playCount += count; }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Error: Terjadi overflow pada playCount! Angka terlalu besar.");
+            }
         }
 
         public void PrintTrackDetails()
         {
-            Console.WriteLine($"ID: {this.id}");
-            Console.WriteLine($"Title: {this.title}");
-            Console.WriteLine($"Play Count: {this.playCount}");
-            Console.WriteLine("----------------------------");
+            Console.WriteLine($"ID: {this.id} | Title: {this.title} | Play Count: {this.playCount}");
         }
+    }
 
+    class Program
+    {
         static void Main(string[] args)
         {
-            SayaMusicTrack track1 = new SayaMusicTrack("Kenangan Manis");
-            track1.IncreasePlayCount(200);
-            track1.PrintTrackDetails();
+            List<SayaMusicTrack> playlist = new List<SayaMusicTrack>();
 
-            SayaMusicTrack track2 = new SayaMusicTrack("Bunga Abadi");
-            track2.IncreasePlayCount(124);
-            track2.PrintTrackDetails();
+            playlist.Add(new SayaMusicTrack("Kenangin Manis"));
+            playlist.Add(new SayaMusicTrack("Bungan Abadi"));
+            playlist.Add(new SayaMusicTrack("Monolog"));
+            playlist.Add(new SayaMusicTrack("Glory MUTD"));
+            playlist.Add(new SayaMusicTrack("Sial - Mahalini"));
 
-            SayaMusicTrack track3 = new SayaMusicTrack("Monolog");
-            track3.IncreasePlayCount(324);
-            track3.PrintTrackDetails();
+            Console.WriteLine("=== DAFTAR PLAYLIST AWAL ===");
+            foreach (var track in playlist)
+            {
+                track.PrintTrackDetails();
+            }
 
-            SayaMusicTrack track4 = new SayaMusicTrack("Glori Man UNTD");
-            track4.IncreasePlayCount(2);
-            track4.PrintTrackDetails();
+            Console.WriteLine("\n=== UPDATE PLAY COUNT ===");
 
-            Console.WriteLine("Tekan tombol apapun untuk keluar.");
+            playlist[0].IncreasePlayCount(500000);
+
+            playlist[1].IncreasePlayCount(400000);
+
+            playlist[2].IncreasePlayCount(700000);
+
+            playlist[3].IncreasePlayCount(10000000);
+
+            Console.WriteLine("\n=== SIMULASI OVERFLOW PADA: " + playlist[4].GetType().Name + " ===");
+            for (int i = 0; i < 220; i++)
+            {
+                playlist[4].IncreasePlayCount(10000000);
+            }
+
+            Console.WriteLine("\n=== HASIL AKHIR SEMUA TRACK ===");
+            foreach (var track in playlist)
+            {
+                track.PrintTrackDetails();
+            }
+
+            Console.WriteLine("\nTekan tombol apapun untuk keluar.");
             Console.ReadKey();
         }
     }
